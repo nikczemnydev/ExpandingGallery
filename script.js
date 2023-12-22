679fb79b95649d90575db2d279c5884f9b2cb66f
Rafal <136376818+nikczemnydev@users.noreply.github.com>
2023-12-22 19:57:52 +0100

    Updated comments in JavaScript file.

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
+// grab all panel classes
 const panels = document.querySelectorAll('.panel')
 
+// loop through the node list
 panels.forEach((panel) => {
+    // listen for a click
     panel.addEventListener('click', () => {
         removeActive()
+        // if clicked -> add a class of active (so also flex = 5)
         panel.classList.add("active")
     })
 })
 
+// function to remove active class from elements
 function removeActive() {
+    // loop through panels
     panels.forEach(panel => {
+        // remove active classes from panel objects
         panel.classList.remove('active')
     })
 }
